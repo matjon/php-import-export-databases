194c26fbf21be8685b7e6c2db6b8c676f0752938
Mateusz Jończyk <mat.jonczyk@o2.pl>
2018-05-05 09:11:14 +0200

    export.php: remove whitespace at end of lines

--- export.php
+++ export.php
@@ -1,12 +1,12 @@
 <?php
-  
+
   class exportBD{
     public $host = "";
     public $user = "";
     public $pass = "";
     public $name = "";
     public $tables = '*';
-    
+
     // Constructor
     public function __construct($host,$user,$pass,$name, $tables='*'){
       $this->host = $host;
@@ -15,7 +15,7 @@
       $this->name = $name;
       $this->tables = $tables;
     }
-    
+
     /* backup the db OR just a table */
     public function start(){
       $host = $this->host;
@@ -28,7 +28,7 @@
       $link = mysql_connect($host,$user,$pass);
       mysql_set_charset('utf8mb4', $link);
       mysql_select_db($name,$link);
-      
+
       //get all of the tables
       if($tables === '*')
       {
@@ -43,23 +43,23 @@
       {
           $tables = is_array($tables) ? $tables : explode(',',$tables);
       }
-      
+
       //cycle through
       foreach($tables as $table)
       {
           $result = mysql_query('SELECT * FROM '.$table);
           $num_fields = mysql_num_fields($result);
-          
+
           $return.= 'DROP TABLE '.$table.';';
           $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
           $return.= "\n\n".$row2[1].";\n\n";
-          
-          for ($i = 0; $i < $num_fields; $i++) 
+
+          for ($i = 0; $i < $num_fields; $i++)
           {
               while($row = mysql_fetch_row($result))
               {
                   $return.= 'INSERT INTO '.$table.' VALUES(';
-                  for($j=0; $j < $num_fields; $j++) 
+                  for($j=0; $j < $num_fields; $j++)
                   {
                       $row[$j] = mysql_real_escape_string($row[$j], $link);
                       if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
@@ -70,7 +70,7 @@
           }
           $return.="\n\n\n";
       }
-      
+
       //save file
       $fileName = 'db-backup-'.time().'-'.(md5(implode(',',$tables))).'.sql';
       $handle = fopen($fileName,'w+');
@@ -81,6 +81,6 @@
     }
     
   }
-    
+
 
 ?>
